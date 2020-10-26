a5edad17ebce3c4a8043d056d38d7cdb1acef5cc
Devon DeRaad <deraad@oxy.edu>
2020-10-26 17:57:28 -0400

    Add files via upload

---
+++ r2vim/increase.correlations/temp.3L.r2vim.array.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --job-name=temp.3L.r2vim             # Job Name
+#SBATCH --nodes=1             # nodes
+#SBATCH --array=1-10
+#SBATCH --cpus-per-task=1               # CPU allocation per Task
+#SBATCH --partition=bi            # Name of the Slurm partition used
+#SBATCH --chdir=/home/d669d153/scratch/r2vim        # Set working d$
+#SBATCH --mem-per-cpu=30gb            # memory requested
+#SBATCH --time=20000
+
+module load R
+R -e "Sys.setenv(RSTUDIO_PANDOC='/panfs/pfs.local/work/bi/bin/pandoc/bin');  rmarkdown::render('temp.3L.${SLURM_ARRAY_TASK_ID}.r2vim.Rmd',output_file='temp.3L.${SLURM_ARRAY_TASK_ID}.r2vim.results.html')"
+
